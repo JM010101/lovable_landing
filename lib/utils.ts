8985d21770538beea1051c48cd8f6748fa6f1148
JM010101 <applebig909090@outlook.com>
2026-02-17 15:53:19 -0500

    init

---
+++ lib/utils.ts
@@ -0,0 +1,12 @@
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${month} ${day}, ${year}`;
+}
+
+export function formatNumber(num: number): string {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
